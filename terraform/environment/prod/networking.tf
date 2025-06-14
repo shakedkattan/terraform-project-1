20516d4552bfb1a5f3632125b7d9ff3921468161
shakedkattan <shakedkattan@outlook.com>
2025-06-14 11:11:30 -0400

    Shaked Kattan 14.6 - Networking maybe completed

---
+++ terraform/environment/prod/networking.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+  source = "git::https://github.com/shakedkattan/projects.git//terraform/modules/vpc?ref=main"
+
+  name                = "shaked-vpc"
+  vpc_cidr            = "10.0.0.0/16"
+  public_subnet_cidr  = "10.0.1.0/24"
+  private_subnet_cidr = "10.0.2.0/24"
+  public_subnet_az    = "us-east-1a"
+  private_subnet_az   = "us-east-1a"
+}
