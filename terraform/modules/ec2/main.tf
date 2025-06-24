2d053b5d3cfb8b2513d1ca709d920cdc686a4958
shakedkattan <shakedkattan@outlook.com>
2025-06-24 16:16:45 +0300

    Update main.tf

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "main" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.security_group_ids
 
   tags = merge(
     var.tags,
